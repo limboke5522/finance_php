8cc40b6ac842f0a9d1d625094a92ab45deb3bdb1
thejane <janekoo_@hotmail.com>
2019-02-01 14:35:04 +0700

    new

--- home.php
+++ home.php
@@ -1,14 +1,49 @@
-<div id="page-content-wrapper">
 
-
-                <div align="center">
-                    <h1>Hello THEJANE</h1>
-                    
-                </div>
-            </div> <!-- /#page-content-wrapper -->
 
  
 
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img  src='images/img1.png' alt="First slide" width="100%"  height="400px">
+    </div>
+    <div class="carousel-item">
+      <img  src='images/img2.jpg' alt="Second slide" width="100%"  height="400px">
+    </div>
+    <div class="carousel-item">
+      <img  src='images/img3.jpg' alt="Third slide" width="100%"  height="400px">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
 
+<div id="page-content-wrapper">
+                <div align="center">
+                    <h1>Hello THEJANE</h1>
+                    <h1>Hello THEJANE</h1>
+                    <h1>Hello THEJANE</h1>
+                    <h1>Hello THEJANE</h1>
+
+                    <h1>Hello THEJANE</h1>
+
+                    <h1>Hello THEJANE</h1>
+                    <h1>Hello THEJANE</h1>
+                    <h1>Hello THEJANE</h1>
+
+                    <h1>Hello THEJANE</h1>
+                    v
+                    
+                
+
+                    
+                </div>
+            </div> <!-- /#page-content-wrapper -->
 
--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 </head>
        
         
-        <nav class="navbar navbar-expand navbar-dark c" > <a href="#menu-toggle" id="menu-toggle" class="navbar-brand"><span class="navbar-toggler-icon"></span></a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+        <nav class="navbar navbar-expand navbar-dark c " > <a href="#menu-toggle" id="menu-toggle" class="navbar-brand"><span class="navbar-toggler-icon"></span></a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
             <div class="collapse navbar-collapse" id="navbarsExample02" >
                 <ul class="navbar-nav mr-auto" class="c">
                     <li class="nav-item active" class="c"> <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> </li>
