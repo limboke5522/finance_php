599ca9a7e79c638c50a334f538e7980297e28e66
thejane <janekoo_@hotmail.com>
2019-02-04 15:35:52 +0700

    1

--- home.php
+++ home.php
@@ -28,18 +28,7 @@
 <div id="page-content-wrapper">
                 <div align="center">
                     <h1>Hello THEJANE</h1>
-                    <h1>Hello THEJANE</h1>
-                    <h1>Hello THEJANE</h1>
-                    <h1>Hello THEJANE</h1>
-
-                    <h1>Hello THEJANE</h1>
-
-                    <h1>Hello THEJANE</h1>
-                    <h1>Hello THEJANE</h1>
-                    <h1>Hello THEJANE</h1>
-
-                    <h1>Hello THEJANE</h1>
-                    v
+              
                     
                 
 
